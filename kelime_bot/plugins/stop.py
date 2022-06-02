32697ebe10e5d3f36e573ea53aa6b4a769d81280
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-06-02 14:29:27 +0300

    Update stop.py

--- kelime_bot/plugins/stop.py
+++ kelime_bot/plugins/stop.py
@@ -15,12 +15,12 @@ async def stop(c:Client, m:Message):
     
     siralama = []
     for i in oyun[m.chat.id]["oyuncular"]:
-        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} 𝖯𝗎𝖺𝗇")
+        siralama.append(f"{i}  :   {oyun[m.chat.id]['oyuncular'][i]} 𝖯𝗎𝖺𝗇")
     siralama.sort(reverse=True)
     siralama_text = ""
     for i in siralama:
         siralama_text += i + "\n"     
     
-    await c.send_message(m.chat.id, f"**{m.from_user.mention}** 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 ! \n 𝖮𝗒𝗎𝗇 𝖡𝗂𝗍𝗍𝗂𝗋𝗂𝗅𝖽𝗂 ! \n 𝖸𝖾𝗇𝗂 𝖮𝗒𝗎𝗇 𝖡𝖺𝗌𝗅𝖺𝗍𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 \n /game 𝖸𝖺𝗓𝖺𝖻𝗂𝗅𝗂𝗋𝗌𝗂𝗇𝗂𝗓 . . .\n\n 📝 𝖯𝗎𝖺𝗇 𝖳𝖺𝖻𝗅𝗈𝗌𝗎  :\n\n{siralama_text}")
+    await c.send_message(m.chat.id, f"**{m.from_user.mention}** 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 ! \n•> 𝖪𝖾𝗅𝗂𝗆𝖾 𝖮𝗒𝗎𝗇𝗎 𝖡𝗂𝗍𝗍𝗂𝗋𝗂𝗅𝖽𝗂 ! \n\n•> 𝖸𝖾𝗇𝗂 𝖮𝗒𝗎𝗇 𝖡𝖺𝗌𝗅𝖺𝗍𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 \n /game 𝖸𝖺𝗓𝖺𝖻𝗂𝗅𝗂𝗋𝗌𝗂𝗇𝗂𝗓 . . .\n\n 🏆  𝖯𝗎𝖺𝗇 𝖳𝖺𝖻𝗅𝗈𝗌𝗎  :\n\n{siralama_text}")
     oyun[m.chat.id] = {}
     
