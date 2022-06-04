7920992f4c9d185c1e223548c04d1e604e7948d4
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-06-04 09:44:41 +0300

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup
 kanal = InlineKeyboardMarkup([
     [
     InlineKeyboardButton("📢 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 " , url= "t.me/Starbotkanal/7"),
-    InlineKeyboardButton("🎯 𝖪𝗎𝗋𝖾𝗌𝖾𝗅 " , url= "t.me/Haribotube/6")
+    InlineKeyboardButton("🏆 𝖪𝗎𝗋𝖾𝗌𝖾𝗅 " , url= "t.me/Haribotube/6")
     ],[
     InlineKeyboardButton("🇹🇷 𝖲𝗈𝗁𝖻𝖾𝗍 𝖦𝗋𝗎𝖻𝗎 " , url= "t.me/SohbetGirdap")
     ]
