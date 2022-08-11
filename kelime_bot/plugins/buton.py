b82cd50d3974349ff6c88222eea25b429e0a801c
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-08-11 19:01:40 +0300

    Update buton.py

--- kelime_bot/plugins/buton.py
+++ kelime_bot/plugins/buton.py
@@ -10,9 +10,6 @@ keyboard = InlineKeyboardMarkup([
     [
         InlineKeyboardButton("📮 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 ", url="t.me/Starbotdestek"),
         InlineKeyboardButton("📢 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 ", url="t.me/Starbotkanal/7"),
-    ],
-    [
-        InlineKeyboardButton("🏆 𝖤𝗇 𝗂𝗒𝗂 20 𝖮𝗒𝗎𝗇𝖼𝗎 ", url="t.me/HariboTube/6"),
     ]
 ])
 
