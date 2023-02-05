dbc70ba99bb21cbe89168627eec254dcee80fa10
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-02-05 21:25:47 +0300

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -3,8 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
     [
-    InlineKeyboardButton("📢 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 " , url= "t.me/armancbey_benim/5"),
-    InlineKeyboardButton("📮 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 " , url= "t.me/SohbetParis")
+    InlineKeyboardButton("📖 Komutlar 📖" , url= "t.me/KelimeTR/5"),
     ]
 
 ])
