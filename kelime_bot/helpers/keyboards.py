2b6d32950e3e3fff6e2c25b85d04c99277e34470
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-02-07 16:44:24 +0300

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -8,7 +8,7 @@ kanal = InlineKeyboardMarkup([
 
 ])
 
-deste = InlineKeyboardMarkup([
+destek = InlineKeyboardMarkup([
     [
     InlineKeyboardButton("♻️ Küresel Skor ♻️" , url= "t.me/KelimeTR/7"),
     ]
