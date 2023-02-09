dc68c64a8910470250eb056e8a3cb8ed272f1fb3
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-02-09 14:35:11 +0300

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -7,20 +7,20 @@ from kelime_bot.helpers.keyboards import *
 
 @Client.on_message(filters.command("skor"))
 async def ratingsa(c:Client, m:Message):
-    metin = """🎖️  Global Top 20  🎖️
+    metin = """**🎖️  Global Top 20  🎖️**
 
 """
     eklenen = 1
     s = sorted(rating.items(), key=lambda x: x[1], reverse=True)
     for kisi in s:
         if eklenen == 1:
-            metin +=  f"🥇 » {kisi[0]} : {kisi[1]} **Puan**\n" 
+            metin +=  f"**🥇 {kisi[0]} : {kisi[1]} Puan**\n" 
         if eklenen == 2:
-            metin +=  f"🥈 » {kisi[0]} : {kisi[1]} **Puan**\n"
+            metin +=  f"**🥈 {kisi[0]} : {kisi[1]} Puan**\n"
         if eklenen == 3:
-            metin +=  f"🥉 » {kisi[0]} : {kisi[1]} **Puan**\n"
+            metin +=  f"**🥉 {kisi[0]} : {kisi[1]} Puan**\n"
         if  not eklenen in [1,2,3]:
-            metin +=  f" {eklenen} » {kisi[0]} : {kisi[1]} **Puan**\n" 
+            metin +=  f"** {eklenen}) {kisi[0]} : {kisi[1]} Puan**\n" 
         eklenen+=1
         if eklenen == 21:
             break
