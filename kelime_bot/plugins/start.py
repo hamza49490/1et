efbb059e9d31b2b5f8709f1e6c1568443dd06199
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-02-09 14:14:23 +0300

    Update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -6,7 +6,7 @@ from kelime_bot import oyun
 from kelime_bot.helpers.kelimeler import *
 from kelime_bot.helpers.keyboards import *
 
-@Client.on_message(filters.command("turet") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("baslat") & ~filters.private & ~filters.channel)
 async def kelimeoyun(c:Client, m:Message):
     global oyun
     aktif = False
@@ -17,9 +17,9 @@ async def kelimeoyun(c:Client, m:Message):
         aktif = False
 
     if aktif:
-        await m.reply("•> 𝖹𝖺𝗍𝖾𝗇 𝗌𝗎 𝖺𝗇𝖽𝖺 𝖽𝖾𝗏𝖺𝗆 𝖾𝖽𝖾𝗇 𝖻𝗂𝗋 𝗈𝗒𝗎𝗇 𝗏𝖺𝗋 ✍🏻 \n• 𝖮𝗒𝗎𝗇𝗎 𝖽𝗎𝗋𝖽𝗎𝗋𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 /stop 𝗄𝗈𝗆𝗎𝗍𝗎𝗇𝗎 𝗄𝗎𝗅𝗅𝖺𝗇𝗂𝗇 ✓", reply_markup=destek)
+        await m.reply("**💭 Zaten Aktif Oyun Var .\n♻️ Durdurmak için /kapat Yazın .**", reply_markup=destek)
     else:
-        await m.reply(f"**{m.from_user.mention}** 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 ! \n𝖪𝖾𝗅𝗂𝗆𝖾 𝖮𝗒𝗎𝗇𝗎 𝖡𝖺𝗌𝗅𝖺𝗍𝗂𝗅𝖽𝗂 .\n\n•> 𝖨𝗒𝗂 𝖮𝗅𝖺𝗇 𝖪𝖺𝗓𝖺𝗇𝗌𝗂𝗇 😈 😈", reply_markup=kanal)
+        await m.reply(f"**{m.from_user.mention} Tarafından .\n💡 Oyun Başlatıldı .\n\n♻️ Hızlı Olan Kazanır :)**", reply_markup=kanal)
         
         oyun[m.chat.id] = {"kelime":kelime_sec()}
         oyun[m.chat.id]["aktif"] = True
@@ -34,12 +34,13 @@ async def kelimeoyun(c:Client, m:Message):
         for harf in kelime:
             kelime_list+= harf + " "
         
-        text = f"""
-🎯 𝖱𝖺𝗎𝗇𝖽 : {oyun[m.chat.id]['round']}/60
-📝 𝖪𝖾𝗅𝗂𝗆𝖾 :   <code>{kelime_list}</code>
-💰 𝖪𝖺𝗓𝖺𝗇𝖽𝗂𝗋𝖺𝖼𝖺𝗄 𝖯𝗎𝖺𝗇 : 1
-🔎 İ𝗉𝗎𝖼𝗎 : 1. {oyun[m.chat.id]["kelime"][0]}
-✍🏻 𝖴𝗓𝗎𝗇𝗅𝗎𝗄 : {int(len(kelime_list)/2)} 
-✏️ 𝖪𝖺𝗋𝗂𝗌𝗂𝗄 𝖧𝖺𝗋𝖿𝗅𝖾𝗋𝖽𝖾𝗇 𝖣𝗈𝗀𝗋𝗎 𝖪𝖾𝗅𝗂𝗆𝖾𝗒𝗂 𝖡𝗎𝗅𝗎𝗇 🥳 🥳 🥳
-        """
+        text = f"""**
+🎯 Raund : {oyun[m.chat.id]['round']}/60 
+📖 Kelime :   <code>{kelime_list}</code>
+💰 Kazandıracak Puan : 1
+🔎 İpucu : 1. {oyun[m.chat.id]["kelime"][0]}
+🌟 Uzunluk : {int(len(kelime_list)/2)} 
+
+♻️ Karışık Harflerden Doğru Kelimeyi Bulun .
+            **"""
         await c.send_message(m.chat.id, text)
