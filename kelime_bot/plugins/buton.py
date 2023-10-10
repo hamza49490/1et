c3f0274c045e417b286feaa3c8d1565566fcb45a
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 01:42:13 +0300

    Update buton.py

--- kelime_bot/plugins/buton.py
+++ kelime_bot/plugins/buton.py
@@ -1,22 +1,22 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardMarkup
-from pyrogram.types import InlineKeyboardButton
+#from pyrogram import Client, filters
+#from pyrogram.types import Message
+#from pyrogram.types import InlineKeyboardMarkup
+#from pyrogram.types import InlineKeyboardButton
 
-keyboard = InlineKeyboardMarkup([
-    [
-        InlineKeyboardButton("✅  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  ✅", url=f"http://t.me/Kelimex_Bot?startgroup=new")
-    ],
-    [
-        InlineKeyboardButton("📢 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 ", url="t.me/KelimeTR"),
-    ]
-])
+#keyboard = InlineKeyboardMarkup([
+#    [
+#        InlineKeyboardButton("✅  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  ✅", url=f"http://t.me/Kelimex_Bot?startgroup=new")
+#    ],
+#    [
+#        InlineKeyboardButton("📢 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 ", url="t.me/KelimeTR"),
+#    ]
+#])
 
 
 
-START = """
-👋🏻 **Merhaba** .\n\n🎮 **Ben Bir Oyun Botuyum** . \n\n🎲 **Çeşitli oyunlar oynamak ve eğlenceli vakit geçirmek için benimle oynayabilirsin** . \n\n⏳ **Benimle oynamak için beni bir gruba ekleyip yönetici yapman lazim** .
-"""
+#START = """
+#👋🏻 **Merhaba** .\n\n🎮 **Ben Bir Oyun Botuyum** . \n\n🎲 **Çeşitli oyunlar oynamak ve eğlenceli vakit geçirmek için benimle oynayabilirsin** . \n\n⏳ **Benimle oynamak için beni bir gruba ekleyip yönetici yapman lazim** .
+#"""
 
     
     
@@ -24,9 +24,9 @@ START = """
     
     
     
-"""
-PRIVATE /start MESSAGE
-"""
-@Client.on_message(filters.command("ndnjr") & filters.private)
-async def priv_start(c:Client, m:Message):
-    await c.send_message(m.chat.id, START, reply_markup=keyboard)
+#"""
+#PRIVATE /start MESSAGE
+#"""
+#@Client.on_message(filters.command("start") & filters.private)
+#async def priv_start(c:Client, m:Message):
+#    await c.send_message(m.chat.id, START, reply_markup=keyboard)
