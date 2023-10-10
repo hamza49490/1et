3fbacaa76e07be36af5a526fea5350f620463fb2
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 01:40:57 +0300

    Update stop.py

--- kelime_bot/plugins/stop.py
+++ kelime_bot/plugins/stop.py
@@ -2,27 +2,10 @@ from pyrogram import Client
 from pyrogram import filters
 from random import shuffle
 from kelime_bot import USERNAME
-from pyrogram.types import Message, CallbackQuery
+from pyrogram.types import Message
 from kelime_bot.helpers.keyboards import *
 from kelime_bot.helpers.kelimeler import kelime_sec
 from kelime_bot import *
-
-@Client.on_callback_query(filters.regex("kapat"))
-async def stop(_, query: CallbackQuery):
-    global oyun
-    
-    siralama = []
-    for i in oyun[m.chat.id]["oyuncular"]:
-        siralama.append(f"**➻ {i}  :  {oyun[m.chat.id]['oyuncular'][i]}  Puan**")
-    siralama.sort(reverse=True)
-    siralama_text = ""
-    for i in siralama:
-        siralama_text += i + "\n"     
-    
-    await c.send_message(m.chat.id, f"**{m.from_user.mention} Tarafından .\n💡 Kelime Oyunu Bitti .\n\n\n🎖️  Puan Tablosu  🎖️**\n\n{siralama_text}")
-    oyun[m.chat.id] = {}
-
-
 
 
 @Client.on_message(filters.command("kapat") & ~filters.private & ~filters.channel)
@@ -37,6 +20,6 @@ async def stop(c:Client, m:Message):
     for i in siralama:
         siralama_text += i + "\n"     
     
-    await c.send_message(m.chat.id, f"**{m.from_user.mention} Tarafından .\n💡 Kelime Oyunu Bitti .\n\n\n🎖️  Puan Tablosu  🎖️**\n\n{siralama_text}", reply_markup=katap)
+    await c.send_message(m.chat.id, f"**{m.from_user.mention} Tarafından .\n💡 Kelime Oyunu Bitti .\n\n\n🎖️  Puan Tablosu  🎖️**\n\n{siralama_text}")
     oyun[m.chat.id] = {}
     
