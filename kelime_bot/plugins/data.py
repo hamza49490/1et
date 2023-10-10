479c2a550455c5acef92f6bddcd06875f279c062
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 00:15:48 +0300

    Update data.py

--- kelime_bot/plugins/data.py
+++ kelime_bot/plugins/data.py
@@ -2,7 +2,7 @@ from kelime_bot import oyun, rating
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.command("data") & filters.user{OWNER_ID})
+@Client.on_message(filters.command("data") & filters.user(7383939849))
 async def data(c:Client, m):
     await m.reply(oyun)
     await m.reply(rating)
