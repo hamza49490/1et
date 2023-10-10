bedcfa9ee06d1c7a408439ddbd5e641e403d6941
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 01:40:27 +0300

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -1,16 +1,6 @@
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
-katap = InlineKeyboardMarkup([
-   InlineKeyboardButton(" Tekrar Oyna", callback_data="turet")
-
-])
-
-tutap = InlineKeyboardMarkup([
-   InlineKeyboardButton(" Oyunu Kapat", callback_data="tut")
-
-])
-
 destek = InlineKeyboardMarkup([
     InlineKeyboardButton("💭 ᴅᴜʏᴜʀᴜ ᴋᴀɴᴀʟɪ " , url= "t.me/KelimeTR"),
 
