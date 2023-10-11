dfa099a4b5c9a4278eafc2e913d6b1e2819cde08
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 03:57:57 +0300

    Update pass.py

--- kelime_bot/plugins/pass.py
+++ kelime_bot/plugins/pass.py
@@ -44,6 +44,5 @@ async def passs(c:Client, m:Message):
             await c.send_message(m.chat.id, text)
             
         else:
-    else:
             await c.send_message(m.chat.id, f"**💭 Pass Hakkın Tükendi .\n➻ Oyunu Bitirmek için ➡️ /kapat**")
         await m.reply(f"**💭 Aktif Oyun Yok .\n➻ Yeni Oyun için ➡️ /turet**")
