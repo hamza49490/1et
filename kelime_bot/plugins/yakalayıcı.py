f9b186c8e309bea01b51defcb4a3779017d04597
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 05:57:55 +0300

    Update yakalayıcı.py

--- kelime_bot/plugins/yakalayıcı.py
+++ kelime_bot/plugins/yakalayıcı.py
@@ -16,7 +16,7 @@ async def buldu(c:Client, m:Message):
     try:
         if m.chat.id in oyun:
             if m.text.lower().replace(" ","") == oyun[m.chat.id]["kelime"]:
-                await c.send_message(m.chat.id,f"**{m.from_user.mention} \n{oyun[m.chat.id]['kelime']} , Kelimesini Buldu 🎯**")
+                await c.send_message(m.chat.id,f"**➻ {m.from_user.mention} \n{oyun[m.chat.id]['kelime']} Kelimesini Buldu !**")
                 if f"{m.from_user.mention}" in rating:
                     rating[f"{m.from_user.mention}"] += 1
                 else:
