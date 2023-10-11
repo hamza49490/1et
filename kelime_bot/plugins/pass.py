fda8e68e43b6dc9c7a73c601ed4a54fc2c572d2c
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 06:19:22 +0300

    Update pass.py

--- kelime_bot/plugins/pass.py
+++ kelime_bot/plugins/pass.py
@@ -21,7 +21,7 @@ async def passs(c:Client, m:Message):
     if aktif:
         if oyun[m.chat.id]["pass"] < 5:
             oyun[m.chat.id]["pass"] += 1 
-            #await c.send_message(m.chat.id,f"**➻ Toplam 5 Pass Hakkın Var .**\n🗯️ `{oyun[m.chat.id]['kelime']}` **Pas Geçildi .**")
+           # await c.send_message(m.chat.id,f"**➻ Toplam 5 Pass Hakkın Var .**\n🗯️ `{oyun[m.chat.id]['kelime']}` **Pas Geçildi .**")
             
             oyun[m.chat.id]["kelime"] = kelime_sec()
             oyun[m.chat.id]["aktif"] = True
@@ -44,7 +44,7 @@ async def passs(c:Client, m:Message):
 
 👁️‍🗨️ Karışık Harflerden Doğru Kelimeyi Bulun . . .
             **"""
-            #await c.send_message(m.chat.id, text)
+            await c.send_message(m.chat.id, text)
             
         else:
             await c.send_message(m.chat.id, f"**💭 Pass Hakkın Tükendi .\n➻ Oyunu Bitirmek için ➡️ /kapat**")
