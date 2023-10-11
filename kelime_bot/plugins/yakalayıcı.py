46a466f8479447eb20baff3b8ba4e7e09a2e0354
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-11 18:48:08 +0300

    Update yakalayıcı.py

--- kelime_bot/plugins/yakalayıcı.py
+++ kelime_bot/plugins/yakalayıcı.py
@@ -16,7 +16,7 @@ async def buldu(c:Client, m:Message):
     try:
         if m.chat.id in oyun:
             if m.text.lower().replace(" ","") == oyun[m.chat.id]["kelime"]:
-                await c.send_message(m.chat.id,f"**➻ {m.from_user.mention} **\n`{oyun[m.chat.id]['kelime']}` **Kelimesini Buldu !**")
+                #await c.send_message(m.chat.id,f"**➻ {m.from_user.mention} **\n`{oyun[m.chat.id]['kelime']}` **Kelimesini Buldu !**")
                 if f"{m.from_user.mention}" in rating:
                     rating[f"{m.from_user.mention}"] += 1
                 else:
@@ -51,7 +51,10 @@ async def buldu(c:Client, m:Message):
                 for harf in kelime:
                     kelime_list+= harf + " "
             
-                text = f"""**🎯 Raund : {oyun[m.chat.id]['round']}/80 
+                text = f"""**➻ {m.from_user.mention} **
+`{oyun[m.chat.id]['kelime']}` **Kelimesini Buldu !
+                
+🎯 Raund : {oyun[m.chat.id]['round']}/80 
 📖 Kelime :   <code>{kelime_list}</code>
 💰 Kazandıracak Puan : 1
 🔎 İpucu : 1. {oyun[m.chat.id]["kelime"][0]}
