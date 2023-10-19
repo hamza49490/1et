88d3747aa0efa46f4b6f07d8ac1a03f2fb4fd205
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-19 06:32:00 +0300

    Update __init__.py

--- kelime_bot/__init__.py
+++ kelime_bot/__init__.py
@@ -3,20 +3,16 @@ from pyrogram import Client
 import logging
 
 
-# THE LOGGING
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
-
-# Hesap
 API_ID = "16347484"
 API_HASH = "da52627a852e4d62633fd6ab72dee4c4"
 TOKEN = "6404904263:AAHP25SjaF85qCncHTq5NE9zA4A-ASD5XNA"
 USERNAME = "BuketTaggerBot"
-
 OWNER_ID = 6540285284
 
 # BOT CLIENTİ
@@ -30,10 +26,5 @@ app = Client(
     )
 
 
-# Oyun Verileri
 oyun = {}
-
-
-# rating
 rating = {}
-
