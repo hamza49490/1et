5ff8509884e404a2ee4fe5461d0b4a6d37fb42eb
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-19 22:27:25 +0300

    Update del.py

--- del.py
+++ del.py
@@ -3,7 +3,7 @@ import requests
 import random
 from datetime import datetime
 
-bot_token = '6559325433:AAF-G05bNjC-S5TwbmW222eY77SU8jM5GhY'
+bot_token = '6559325433:AAHRdRuS7agUSYXIYpQPfS7gYvLO5tXNPyY'
 
 bot_active = False
 
