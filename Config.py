e3be7810633cbdc4288928d678b90d6d28fa7f88
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-19 06:32:09 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,1 +1,18 @@
+import os
 
+class Config:
+
+API_ID = int(os.environ.get("API_ID","18049084"))
+API_HASH = os.environ.get("API_HASH","7e74b1e22026fcc291d32b3d431aa21e")
+BOT_TOKEN = os.environ.get("TOKEN","6404904263:AAHP25SjaF85qCncHTq5NE9zA4A-ASD5XNA")
+BOT_ID = int(os.environ.get("BOT_ID", "6404904263"))
+DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://epiktv7:epiktv86@cluster0.ttyjqmj.mongodb.net/?retryWrites=true&w=majority") # MongoDB veritabanınızın url'si. Nasıl alacağınızı bilmiyorsanız destek grubu @RepoHaneX'e gelin.
+BOT_USERNAME = os.environ.get("BOT_USERNAME","BuketTaggerBot") # Botunuzun kullanıcı adı.
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001983841726")) # Botunuzun eylemleri kaydedeceği kayıt grubunun id'si.
+GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "BuketBilgi") # Botunuzdan yasaklanan kullanıcıların itiraz işlemleri için başvuracağı grup, kanal veya kullanıcı. Boş bırakırsanız otomatik olarak OWNER_ID kimliğine yönlendirecektir.
+GONDERME_TURU = os.environ.get("GONDERME_TURU", False) # Botunuzun yanıtladığınız mesajı gönderme türü. Eğer direkt iletmek isterseniz False, kopyasını göndermek isterseniz True olarak ayarlayın.
+OWNER_ID = int(os.environ.get("OWNER_ID","6540285284")) # Sahip hesabın id'si
+OWNERNAME = "ㅤᴀɪᴋᴏㅤ"
+OWNER = [6540285284]
+
+LANGAUGE = os.environ.get("LANGAUGE", "TR")
