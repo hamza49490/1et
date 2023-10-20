8f41be05499965ac5117d0ce68f17ad88761a205
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-20 18:44:13 +0300

    Create reload.py

---
+++ kelime_bot/plugins/reload.py
@@ -0,0 +1,17 @@
+import asyncio
+from asyncio import sleep
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, Message
+
+
+@Client.on_message(command(["reload"]))
+async def update_admin(client: Client, message: Message):
+    global admins
+    if message.chat.type == "private":
+        return
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await message.reply_text("**🎄 ʙᴏᴛ ʏᴇɴɪᴅᴇɴ ʙᴀşʟᴀᴅɪ .\n🎄 ᴀᴅᴍɪɴ ʟɪsᴛᴇsɪ ɢᴜ̈ɴᴄᴇʟʟᴇɴᴅɪ .**")
