4df1d76da7555090810732a187c5d4b8e51a0c3b
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-21 08:05:04 +0300

    Update chatgpt.py

--- kelime_bot/plugins/chatgpt.py
+++ kelime_bot/plugins/chatgpt.py
@@ -4,26 +4,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.enums import ChatAction, ParseMode
 import openai
 from pyrogram.types import CallbackQuery
-from config import *
 import os,sys,re,requests
 import asyncio,time
 from random import choice
 from datetime import datetime
 import logging
 
-FORMAT = "[LEGEND-MUKESH] %(message)s"
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-
 
 StartTime = time.time()
-app = Client(
-    "chat-gpt" ,
-    api_id = API_ID,
-    api_hash = API_HASH ,
-    bot_token = BOT_TOKEN
-)
    
 openai.api_key = OPENAI_KEY
 @app.on_message(filters.command(["buket"],  prefixes=["", "/"]))
@@ -45,19 +33,3 @@ async def chat(bot, message):
             await message.reply_text(f"**📑 {x}**", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"** Hata **")
-
-
-if __name__ == "__main__":
-    print(f"""Bot Aktif !""")
-    try:
-        app.start()
-
-    
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("API_ID/API_HASH'ınız geçerli değil.")
-    except AccessTokenInvalid:
-        raise Exception("BOT_TOKEN'iniz geçerli değil.")
-    print(f"""REPO'YA KATILIN !""")
-    idle()
-    app.stop()
-    print("Bot Durdu !")
