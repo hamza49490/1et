32776fb87f5b0d715e088b421e9e50ea79c4d40c
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-22 01:21:49 +0300

    Update song.py

--- kelime_bot/plugins/song.py
+++ kelime_bot/plugins/song.py
@@ -26,24 +26,6 @@ from pyrogram.types import Message, User
 from pyrogram import Client, filters
 from kelime_bot import BOT_ID, BOT_USERNAME, OWNER_ID
 
-# "/lyrics" komutunu işleyen bir filtre oluşturun
-@filters.command("lyrics")
-def get_lyrics(client: Client, message: Message):
-    # Mesajdaki şarkı adını alın
-    song_name = message.text.split("/lyrics", maxsplit=1)[1].strip()
-    
-    # Şarkı sözlerini almak için bir API isteği yapın
-    response = requests.get(f"https://api.lyrics.ovh/v1/{song_name}")
-    
-    # İstek başarılı ise şarkı sözlerini gönderin
-    if response.status_code == 200:
-        lyrics = response.json()["lyrics"]
-        message.reply_text(lyrics)
-    else:
-        message.reply_text("Şarkı sözleri bulunamadı.")
-
-# Mesajları filtreleyin ve "/lyrics" komutunu işleyin
-app.add_handler(get_lyrics)
 
 ##############################
 @Client.on_message(filters.command(["bul", "song"]) & ~filters.edited)
