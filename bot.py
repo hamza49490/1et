378f1db67b2db2f0220c4be537ce4203eddfee71
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-22 05:03:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -928,7 +928,9 @@ async def tag(event):
   
   if event.pattern_match.group(1):
     mode = "text_on_cmd"
-    msg = event.pattern_match.string.replace("\n", " ")
+    msg = event.pattern_match.string.split(None, 1)[1]
+    if msg == "/tag":
+        return await event.respond(f"💭 ʙɪʀ ᴍᴇsᴀᴊ ᴠᴇʀɪɴ .\n💕 ᴏ‌ʀɴᴇᴋ : /tag Merhaba")
   elif event.reply_to_msg_id:
     mode = "text_on_reply"
     msg = event.reply_to_msg_id
@@ -937,7 +939,7 @@ async def tag(event):
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
     return await event.respond("____")
   else:
-    return await event.respond(f"**💭 ʙɪʀ ᴍᴇsᴀᴊ ᴠᴇʀɪɴ .\n💕 ᴏ̈ʀɴᴇᴋ : /tag Merhaba**")
+    return await event.respond(f"💭 ʙɪʀ ᴍᴇsᴀᴊ ᴠᴇʀɪɴ .\n💕 ᴏ‌ʀɴᴇᴋ : /tag Merhaba")
   if mode == "text_on_cmd":
     anlik_calisan.append(event.chat_id)
     usrnum = 0
@@ -946,23 +948,23 @@ async def tag(event):
 
     gece_tag.append(event.chat_id)
     usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
+    usrtxt = ""   
+    async for usr in client.iter_participants(event.chat_id, filter=ChannelParticipantsBots | ChannelParticipantsDeleted):
       rxyzdev_tagTot[event.chat_id] += 1
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.id})"
       if event.chat_id not in gece_tag:
         return
       if usrnum == 1:
-        await client.send_message(event.chat_id, f"**{usrtxt}  {msg}**")
+        await client.send_message(event.chat_id, f"{usrtxt}  {msg}")
         await asyncio.sleep(4)
         usrnum = 0
         usrtxt = ""
      
     sender = await event.get_sender()
     rxyzdev_initT = f"[{sender.first_name}](tg://user?id={sender.id})"      
-    if event.chat_id in rxyzdev_tagTot:await event.respond(f"**🗨️ ᴇᴛɪᴋᴇᴛʟᴇᴍᴇʏɪ ᴛᴀᴍᴀᴍʟᴀᴅɪᴍ ...\n\n➻  {rxyzdev_initT}\n👤 ᴇᴛɪᴋᴇᴛʟᴇʀɪɴ sᴀʏɪsɪ : {rxyzdev_tagTot[event.chat_id]}**")
-
+    if event.chat_id in rxyzdev_tagTot:await event.respond(f"🗨️ ᴇᴛɪᴋᴇᴛʟᴇᴍᴇʏɪ ᴛᴀᴍᴀᴍʟᴀᴅɪᴍ ...\n\n➻  {rxyzdev_initT}\n👤 ᴇᴛɪᴋᴇᴛʟᴇʀɪɴ sᴀʏɪsɪ : {rxyzdev_tagTot[event.chat_id]}")
+	    
 
 @client.on(events.NewMessage(pattern="^/etag ?(.*)"))
 async def etag(event):
