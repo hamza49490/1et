2d108a9819147560f0f7885acd949c7d99c05991
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-22 19:43:27 +0300

    Update doviz.py

--- kelime_bot/plugins/doviz.py
+++ kelime_bot/plugins/doviz.py
@@ -4,7 +4,7 @@ import requests
 # API anahtarını buraya girin
 API_KEY = "bc5ca147175e26ed57581b6b"
 
-# Komutları işlemek için bir filtre oluşturun
+
 @filters.command(["dolar"])
 def get_dolar(update, context):
     message = update.message
@@ -13,16 +13,25 @@ def get_dolar(update, context):
     dolar_kuru = data["rates"]["TRY"]
     message.reply_text(f"1 USD = {dolar_kuru} TRY")
 
+# Çağrıldığı yerde message argümanını ekleyin
+get_dolar(update, context, message)
+
 @filters.command(["euro"])
-def get_euro(_, message):
+def get_euro(update, context):
     response = requests.get(f"https://api.exchangerate-api.com/v4/latest/EUR")
     data = response.json()
     euro_kuru = data["rates"]["TRY"]
     message.reply_text(f"1 EUR = {euro_kuru} TRY")
 
+# Çağrıldığı yerde message argümanını ekleyin
+get_euro(update, context, message)
+
 @filters.command(["altın"])
-def get_altin(_, message):
+def get_altin(update, context):
     response = requests.get(f"https://api.exchangerate-api.com/v4/latest/XAU")
     data = response.json()
     altin_kuru = data["rates"]["TRY"]
     message.reply_text(f"1 XAU = {altin_kuru} TRY")
+
+# Çağrıldığı yerde message argümanını ekleyin
+get_altin(update, context, message)
