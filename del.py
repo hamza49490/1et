df1983bc1bc97a86adeb0f9457f01abbcbf6fefb
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-23 03:15:49 +0300

    Update del.py

--- del.py
+++ del.py
@@ -11,7 +11,28 @@ bot = telebot.TeleBot(TOKEN)
 target_number = None
 start_time = None
 
+@bot.message_handler(commands=['sat'])
+def start(message):
+    global target_number
+    global start_time
 
+    bot.reply_to(message, "Sayı tahmin oyununa hoş geldiniz! 1 ile 100 arasında bir sayı tuttum. Tahmininizi yapabilirsiniz!")
+
+    # Rastgele bir sayı seç
+    target_number = random.randint(1, 100)
+
+    # Oyun süresi başlat
+    start_time = time.time()
+
+@bot.message_handler(commands=['cancel'])
+def cancel(message):
+    global target_number
+    global start_time
+
+    bot.reply_to(message, "Oyun iptal edildi.")
+    target_number = None
+    start_time = None
+    
 @bot.message_handler(func=lambda message: True)
 def guess(message):
     global target_number
