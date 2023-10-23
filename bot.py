f025a4d580467aec4f7fb99039725ee07d03d59c
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-24 01:56:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,8 +85,9 @@ client.storage = {}
 il_plaka_kodlari = {
 'Ağrı': '04', 'Amasya': '05', 'Ankara': '06', 'Antalya': '07', 'Artvin': '08', 'Aydın': '09', 'Balıkesir': '10', 'Bilecik': '11', 'Bingöl': '12', 'Bitlis': '13', 'Bolu': '14', 'Burdur': '15', 'Bursa': '16', 'Çanakkale': '17', 'Çankırı': '18', 'Çorum': '19', 'Denizli': '20', 'Diyarbakır': '21', 'Edirne': '22', 'Elazığ': '23', 'Erzincan': '24', 'Erzurum': '25', 'Eskişehir': '26', 'Gaziantep': '27', 'Giresun': '28', 'Gümüşhane': '29', 'Hakkari': '30', 
 'Hatay': '31', 'Isparta': '32', 'Mersin': '33', 'İstanbul': '34', 'İzmir': '35', 'Kars': '36', 'Kastamonu': '37', 'Kayseri': '38', 'Kırklareli': '39', 'Kırşehir': '40', 'Kocaeli': '41', 'Konya': '42', 'Kütahya': '43', 'Malatya': '44', 'Manisa': '45', 'Kahramanmaraş': '46', 'Mardin': '47', 'Muğla': '48', 'Muş': '49', 'Nevşehir': '50', 'Niğde': '51', 'Ordu': '52', 'Rize': '53', 'Sakarya': '54', 'Samsun': '55', 'Siirt': '56', 'Sinop': '57', 'Sivas': '58', 
-'Tekirdağ': '59', 'Tokat': '60', 'Trabzon': '61', 'Tunceli': '62', 'Şanlıurfa': '63', 'Uşak': '64', 'Van': '65', 'Yozgat': '66', 'Zonguldak': '67', 'Aksaray': '68', 'Bayburt': '69', 'Karaman': '70', 'Kırıkkale': '71', 'Batman': '72', 'Şırnak': '73', 'Bartın': '74', 'Ardahan': '75', 'Iğdır': '76', 'Yalova': '77', 'Karabük': '78', 'Kilis': '79', 'Osmaniye': '80', 'Düzce': '81'
+'Tekirdağ': '59', 'Tokat': '60', 'Trabzon': '61', 'Tunceli': '62', 'Şanlıurfa': '63', 'Uşak': '64', 'Van': '65', 'Yozgat': '66', 'Zonguldak': '67', 'Aksaray': '68', 'Bayburt': '69', 'Karaman': '70', 'Kırıkkale': '71', 'Batman': '72', 'Şırnak': '73', 'Bartın': '74', 'Ardahan': '75', 'Iğdır': '76', 'Yalova': '77', 'Karabük': '78', 'Kilis': '79', 'Osmaniye': '80', 'Düzce': '81', 'Adana': '01', 'Adıyaman': '02', 'Afyonkarahisar': '03',
 }
+
 
 @client.on(events.NewMessage(pattern='/play'))
 async def play(event):
@@ -98,7 +99,7 @@ async def play(event):
     il = random.choice(list(il_plaka_kodlari.keys()))
     plaka_kodu = il_plaka_kodlari[il]
     tur_sayisi = len(client.storage.keys()) - 1
-    await event.respond(f'<b>🚗 Verdiğim şehrin plakasını yazın :)\n\n🏙️ Şehir : {il}\n🎲 Tur : {tur_sayisi}/{tur}</b>', parse_mode='html')
+    await event.respond(f'<b>🚗 Verdiğim şehrin plakasını yazın :)\n\n🏙️ Şehir : {il}\n\n🎲 Tur : {tur_sayisi}/{tur}</b>', parse_mode='html')
     client.storage[plaka_kodu] = {'il': il, 'points': {}}
 
 @client.on(events.CallbackQuery())
@@ -119,7 +120,7 @@ async def guess(event):
         if user_id not in il['points']:
             il['points'][user_id] = 0
         il['points'][user_id] += 1
-        await event.respond(f'💭 <b>Oyun bitti . \n🗯️ Puanınız : {il["points"][user_id]}', parse_mode='html')
+        await event.respond(f'💭 <b>Oyun bitti . \n🗯️ Puanınız : {il["points"][user_id]}</b>', parse_mode='html')
         client.storage.pop(plaka_kodu)  # Sadece tamamlanan turun verilerini temizle
     else:
         if plaka_kodu.isdigit():
