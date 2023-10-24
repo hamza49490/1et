da7b3fa697d80441e8da7053cca9711f771043a0
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-24 05:18:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,8 +58,8 @@ LOGGER = logging.getLogger(__name__)
 
 api_id = int(os.environ.get("APP_ID","26573250"))
 api_hash = os.environ.get("API_HASH","6306d2d23b1083a6f757f64f0b0c609c")
-bot_token = os.environ.get("TOKEN","6559325433:AAECLK5t8nQUFWkI24Hx0NF-ASthWqBo3dU")
-BOT_USERNAME = os.environ.get("BOT_USERNAME","AikoDenemeBot")
+bot_token = os.environ.get("TOKEN","6404904263:AAHP25SjaF85qCncHTq5NE9zA4A-ASD5XNA")
+BOT_USERNAME = os.environ.get("BOT_USERNAME","BuketTaggerBot")
 CHANNELL = os.environ.get("CHANNELL", "BuketBilgi")
 OWNER_ID = int(os.environ.get("OWNER_ID","6540285284"))
 OWNERNAME = "ㅤᴀɪᴋᴏㅤ"
