21b7832ae9b0043641e67bf81f7ef7fb98903c08
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-26 20:23:24 +0300

    Delete kelime_bot directory

--- kelime_bot/helpers/keyboards.py
+++
@@ -1,7 +0,0 @@
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-destek = InlineKeyboardMarkup([
-    InlineKeyboardButton("💭 ᴅᴜʏᴜʀᴜ ᴋᴀɴᴀʟɪ " , url= "t.me/KelimeTR"),
-
-])
