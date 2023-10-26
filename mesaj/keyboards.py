031596af646fb60572c26f4ac3694a7e41354449
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-10-26 21:24:56 +0300

    Add files via upload

---
+++ mesaj/keyboards.py
@@ -0,0 +1,7 @@
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+
+destek = InlineKeyboardMarkup([
+    InlineKeyboardButton("💭 ᴅᴜʏᴜʀᴜ ᴋᴀɴᴀʟɪ " , url= "t.me/KelimeTR"),
+
+])
