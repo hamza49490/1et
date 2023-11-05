86ca9f880d4d2a6766a6678e15e08769d445ece9
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-06 02:27:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -550,24 +550,21 @@ async def slap(event):
 ##################################################
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-    if event.is_private:
-        async for usr in client.iter_participants(event.chat_id):
-            if await event.reply_to(...) is not None:
-                await event.reply_to(
-                    photo="https://telegra.ph/file/9898d0f8006cf4dd47245.jpg",
-                    caption=f"{startmesaj}", 
-                    buttons=(
-                        [
-                            Button.url('➕  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  ➕', f'https://t.me/{BOT_USERNAME}?startgroup=a'),
-                        ],[
-                            Button.inline("📚 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋", data="help"),
-                            Button.url('🗨️ 𝖡𝗂𝗅𝗀𝗂 𝖪𝖺𝗇𝖺𝗅ı', f'https://t.me/{CHANNELL}')
-                        ],[
-                            Button.url('✦  𝖮𝗐𝗇𝖾𝗋  ✦', f'tg://openmessage?user_id={OWNER_ID}')
-                        ]
-                    ),
-                    link_preview=False
-                )
+  if event.is_private:
+    async for usr in client.iter_participants(event.chat_id):
+     await event.reply_photo(
+           photo="https://telegra.ph/file/9898d0f8006cf4dd47245.jpg",
+           caption=f"{startmesaj}", buttons=(
+                      [
+                      Button.url('➕  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  ➕', f'https://t.me/{BOT_USERNAME}?startgroup=a'),
+                    ],[
+                      Button.inline("📚 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋", data="help"),
+                      Button.url('🗨️ 𝖡𝗂𝗅𝗀𝗂 𝖪𝖺𝗇𝖺𝗅ı', f'https://t.me/{CHANNELL}')
+                    ],[
+                      Button.url('✦  𝖮𝗐𝗇𝖾𝗋  ✦', f'tg://openmessage?user_id={OWNER_ID}')
+                    ]
+                  ),
+                link_preview=False)
 
   if event.is_group:
     return await client.send_message(event.chat_id, f"{startmesaj}", buttons=(
