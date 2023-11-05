cd0917725f055905f77895b9b2f0fc00615c1966
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-06 00:50:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -552,15 +552,17 @@ async def slap(event):
 async def start(event):
   if event.is_private:
     async for usr in client.iter_participants(event.chat_id):
-     await event.reply(f"{startmesaj}", buttons=(
+     await event.reply(
+	     photo="https://telegra.ph/file/9898d0f8006cf4dd47245.jpg",
+	     caption=f"{startmesaj}", buttons=(
                       [
                       Button.url('➕  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  ➕', f'https://t.me/{BOT_USERNAME}?startgroup=a'),
                     ],[
                       Button.inline("📚 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋", data="help"),
                       Button.url('🗨️ 𝖡𝗂𝗅𝗀𝗂 𝖪𝖺𝗇𝖺𝗅ı', f'https://t.me/{CHANNELL}')
-		    ],[
-		      Button.url('✦  𝖮𝗐𝗇𝖾𝗋  ✦', f'tg://openmessage?user_id={OWNER_ID}')
-		      ]
+                    ],[
+                      Button.url('✦  𝖮𝗐𝗇𝖾𝗋  ✦', f'tg://openmessage?user_id={OWNER_ID}')
+                    ]
                   ),
                 link_preview=False)
 
