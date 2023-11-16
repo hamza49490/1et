d2350991446aafe21ac32bf21ec06491eda86bfd
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-16 03:26:43 +0300

    Update men.py

--- men.py
+++ men.py
@@ -73,30 +73,25 @@ import lyricsgenius
 # Genius API anahtarını buraya girin
 GENIUS_API_KEY = "PierR-oNNw9tboAn89A9FhbC_boliY9QCuocfcG3QF9OciRtimhp4a6Fnne5lBrm"
 
-if len(message.text.split()) > 1:
-    query = message.text.split(maxsplit=1)[1]
-    # Şarkı sözlerini almak için Genius API'sini kullan
-    genius = lyricsgenius.Genius(GENIUS_API_KEY)
-    # ...
-else:
-    message.reply_text("Lütfen bir şarkı adı girin.")
-# /lyrics komutunu işlemek için bir filtre tanımla
+genius = lyricsgenius.Genius(GENIUS_API_KEY)
+
 @app.on_message(filters.command(["lyrics"], prefixes=['/']))
 def get_lyrics(_, message: Message):
-    # Kullanıcının gönderdiği mesajı al
-    query = message.text.split(maxsplit=1)[1]
-    
-    try:
-        # Şarkı sözlerini Genius API'sinden al
-        song = genius.search_song(query, get_full_info=False)
-        
-        if song:
-            message.reply_text(song.title)
-        else:
-            message.reply_text("Şarkı bulunamadı.")
-    except Exception as e:
-        message.reply_text("Bir hata oluştu.")
-        print(str(e))
+    if len(message.text.split()) > 1:
+        query = message.text.split(maxsplit=1)[1]
+        try:
+            # Şarkı sözlerini Genius API'sinden al
+            song = genius.search_song(query, get_full_info=False)
+            
+            if song:
+                message.reply_text(song.title)
+            else:
+                message.reply_text("Şarkı bulunamadı.")
+        except Exception as e:
+            message.reply_text("Bir hata oluştu.")
+            print(str(e))
+    else:
+        message.reply_text("Lütfen bir şarkı adı girin.")
 
 @app.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]))
 async def start(_, message: Message):
