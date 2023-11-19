baa94968cb5c0e0d4afdb2c38f1096bdd2b4848e
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-19 09:33:28 +0300

    Update men.py

--- men.py
+++ men.py
@@ -297,6 +297,69 @@ rxyzdev_tagTot = {}
 rxyzdev_initT = {} 
 rxyzdev_stopT = {}
 
+@app.on_message(filters.command("tag", prefixes="/"))
+async def utag(client, message):
+    global gece_tag
+    rxyzdev_tagTot[message.chat.id] = 0
+    if message.chat.type == "private":
+        return await message.reply("nogroup")
+  
+    admins = []
+    async for admin in client.iter_chat_members(message.chat.id, filter="administrators"):
+        admins.append(admin.user.id)
+    if message.from_user.id not in admins:
+        return await message.reply("noadmin")
+  
+    if len(message.command) > 1:
+        mode = "text_on_cmd"
+        msg_list = message.text.split(None, 1)
+        if len(msg_list) < 2:
+            return await message.reply("Bir Mesaj Verin")
+        msg = msg_list[1]
+        if msg == "/utag":
+            return await message.reply("Bir Mesaj Verin")
+    elif message.reply_to_message:
+        mode = "text_on_reply"
+        msg = message.reply_to_message.message_id
+        if msg == None:
+            return await message.reply("Bir Mesaj Verin")
+    elif len(message.command) > 1 and message.reply_to_message:
+        mode = "text_on_cmd"
+        msg_list = message.text.split(None, 1)
+        if len(msg_list) < 2:
+            return await message.reply("Bir Mesaj Verin")
+        msg = msg_list[1]
+    else:
+        return await message.reply("Bir Mesaj Verin")
+  
+    if mode == "text_on_cmd":
+        anlik_calisan.append(message.chat.id)
+        usrnum = 0
+        usrtxt = ""
+        await message.reply("ibaslama")
+
+        gece_tag.append(message.chat.id)
+        usrnum = 0
+        usrtxt = ""   
+        async for usr in client.iter_chat_members(message.chat.id):
+            if usr.user.is_bot or usr.user.is_deleted:
+                continue
+            rxyzdev_tagTot[message.chat.id] += 1
+            usrnum += 1
+            usrtxt += f"{usr.user.first_name} , "
+            if message.chat.id not in gece_tag:
+                return
+            if usrnum == 1:
+                await client.send_message(message.chat.id, f"➻ {msg}\n\n{usrtxt}", parse_mode="html")
+                await asyncio.sleep(2)
+                usrnum = 0
+                usrtxt = ""
+     
+    sender = await message.from_user()
+    rxyzdev_initT = f"{sender.first_name}"      
+    if message.chat.id in rxyzdev_tagTot:
+        await message.reply(f"🗨️ ᴇᴛɪᴋᴇᴛʟᴇᴍᴇʏɪ ᴛᴀᴍᴀᴍʟᴀᴅɪᴍ ...\n\n➻  {rxyzdev_initT}\n👤 ᴇᴛɪᴋᴇᴛʟᴇʀɪɴ sᴀʏɪsɪ : {rxyzdev_tagTot[message.chat.id]}", parse_mode="html")
+	    
 @app.on_message(filters.command("utag", prefixes="/"))
 async def utag(client, message):
     global gece_tag
