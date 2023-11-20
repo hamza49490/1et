907e9c9a77d255b2ab851a85c4908689ac7b18f6
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-20 09:03:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "26573250"))
+API_HASH = os.environ.get("API_HASH", "6306d2d23b1083a6f757f64f0b0c609c")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "EpicTaggerBot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6865081184:AAFcDOrflr2l_tibi7tvQ3A9Sv288XxevLA")
+BOT_ID = int(os.environ.get("BOT_ID", "6865081184"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5581058044"))
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mervetopic:topicmerve@cluster0.vpfzgml.mongodb.net/?retryWrites=true&w=majority")
+PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001916993821"))
+MCHANNEL = os.environ.get("MCHANNEL", "MuzikKayit")
+CHANNELL = os.environ.get("CHANNELL", "BotsDuyuru")
+OWNERNAME = "Sahip"
