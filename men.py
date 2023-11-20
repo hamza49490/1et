6d06688e00a52687069ea92279f790e40bff14af
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-20 14:59:53 +0300

    Update men.py

--- men.py
+++ men.py
@@ -185,6 +185,8 @@ async def anonim(_, query):
     mesaj = query.message.text
     gonderen = query.from_user.first_name
     etiraf_eden = "Anonim"
+    log_qrup = "-1001983841726"
+    admin_qrup = "-1001983841726"
     admin_qrup = await app.resolve_peer(admin_qrup)
     yeni_etiraf = await app.send_message(admin_qrup, f"💬 Yeni İtiraf\n\n👤 İtiraf Eden ➻ {etiraf_eden} \n👥 İtirafı ➻ {mesaj} \n\n💌 İtirafınızı {BOT_USERNAME} 'a yazın .")
     tesdiq = await yeni_etiraf.reply("İtiraf Yayınlansın Mı ?", reply_markup=InlineKeyboardMarkup(
@@ -205,6 +207,8 @@ async def aciq(_, query):
     mesaj = query.message.text
     etiraf_eden = query.from_user.first_name
     sonluq = f"\n💌 İtirafınızı {BOT_USERNAME} 'a yazın ."
+    log_qrup = "-1001983841726"
+    admin_qrup = "-1001983841726"
     admin_qrup = await app.resolve_peer(admin_qrup)
     yeni_etiraf = await app.send_message(admin_qrup, f"💬 Yeni İtiraf\n\n👤 İtiraf Eden ➻ {etiraf_eden} \n👥 İtirafı ➻ {mesaj} \n{sonluq}")
     tesdiq = await yeni_etiraf.reply("💬 İtiraf Yayınlansın Mı ?", reply_markup=InlineKeyboardMarkup(
