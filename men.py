88cc2a09331af41b69bbf073f0a66a5f69ba3e63
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-21 07:27:42 +0300

    Update men.py

--- men.py
+++ men.py
@@ -52,7 +52,7 @@ openai.api_key = "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"
 async def chat(bot, message):
     try:
         start_time = time.time()
-        await bot.send_chat_action(message.chat.id, action="typing")
+        await bot.send_chat_action(message.chat.id, action=ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text("✦ ʙᴜʏʀᴜɴ, sᴏʀᴜɴᴜᴢ ɴᴇᴅɪʀ !\n\n☆ öʀɴᴇᴋ : ᴀsᴋ ɢüᴢᴇʟ ʙɪ‌ʀ söᴢ söʏʟᴇ .")
         else:
@@ -66,7 +66,7 @@ async def chat(bot, message):
             )
             x = resp.choices[0].text
             end_time = time.time()
-            await message.reply_text(f"{x}")
+            await message.reply_text(f"{x}", parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
         await message.reply_text(f"✦ ᴅᴀʜᴀ sᴏɴʀᴀ ᴛᴇᴋʀᴀʀ ᴅᴇɴᴇ !")
 	    
