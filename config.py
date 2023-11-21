6bdf189fea6889f81f9c955d6a6f79df947ae36b
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-21 11:48:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,12 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME", "EpicTaggerBot")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6865081184:AAFcDOrflr2l_tibi7tvQ3A9Sv288XxevLA")
 BOT_ID = int(os.environ.get("BOT_ID", "6865081184"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "6181368568"))
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mervetopic:topicmerve@cluster0.vpfzgml.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Kingbrukh:kingkhan@kingbruh.ra3pjgm.mongodb.net/?retryWrites=true&w=majority")
 PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001916993821"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001983841726"))
 MCHANNEL = os.environ.get("MCHANNEL", "MuzikKayit")
 CHANNELL = os.environ.get("CHANNELL", "BotsDuyuru")
 OWNERNAME = "ㅤᴀɪ‌ᴋᴏㅤ"
-LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID", "-1001983841726"))
+GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "BotsDestek") # Botunuzdan yasaklanan kullanıcıların itiraz işlemleri için başvuracağı grup, kanal veya kullanıcı. Boş bırakırsanız otomatik olarak OWNER_ID kimliğine yönlendirecektir.
+GONDERME_TURU = os.environ.get("GONDERME_TURU", False) # Botunuzun yanıtladığınız mesajı gönderme türü. Eğer direkt iletmek isterseniz False, kopyasını göndermek isterseniz True olarak ayarlayın.
+LANGAUGE = os.environ.get("LANGAUGE", "TR")
