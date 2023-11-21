cf8d3fcde6042cdbf4e77d59b80002c6f9f50973
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-21 08:45:19 +0300

    Update men.py

--- men.py
+++ men.py
@@ -85,21 +85,23 @@ async def start(_, message: Message):
         text=f"""✦ Merhaba {message.from_user.mention}""",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✦  𝖡𝗎𝗋𝖺𝗒𝖺 𝖳ı𝗄𝗅𝖺  ✦", url=f"https://t.me/{BOT_USERNAME}?start")]])
     )
-# Kullanıcı id ve adını al
-user_id = message.from_user.id
-user_name = message.from_user.username
+	
+async def send_log_message():
+    # Kullanıcı id ve adını al
+    user_id = message.from_user.id
+    user_name = message.from_user.username
 
-# Grubun kullanıcı id ve adını al
-chat_id = message.chat.id
-chat_name = message.chat.title
+    # Grubun kullanıcı id ve adını al
+    chat_id = message.chat.id
+    chat_name = message.chat.title
 
-# Grubun gizli olup olmadığını kontrol et
-if chat_id == None:
-    chat_name = "private"
+    # Grubun gizli olup olmadığını kontrol et
+    if chat_id == None:
+        chat_name = "private"
 
-# Log grubuna kullanıcı id ve adını bildir
-log_message = f"Kullanıcı ID: {user_id}\nKullanıcı Adı: {user_mention}\nGrup ID: {chat_id}\nGrup Adı: {chat_name}"
-await app.send_message(LOG_GROUP_ID, log_message)
+    # Log grubuna kullanıcı id ve adını bildir
+    log_message = f"Kullanıcı ID: {user_id}\nKullanıcı Adı: {user_mention}\nGrup ID: {chat_id}\nGrup Adı: {chat_name}"
+    await app.send_message(LOG_GROUP_ID, log_message)
 
 @app.on_callback_query(filters.regex("start"))
 async def start(_, query: CallbackQuery):
