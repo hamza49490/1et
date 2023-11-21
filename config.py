0b09c862be3d71dac39267bf3e9435e17089008a
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-21 14:52:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "26573250"))
 API_HASH = os.environ.get("API_HASH", "6306d2d23b1083a6f757f64f0b0c609c")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "EpicTaggerBot")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6865081184:AAFcDOrflr2l_tibi7tvQ3A9Sv288XxevLA")
-BOT_ID = int(os.environ.get("BOT_ID", "6865081184"))
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "DeryaTaggerBot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6654452037:AAGqkStQgz20KOKj8N_3PzWCZfTep13mlig")
+BOT_ID = int(os.environ.get("BOT_ID", "6654452037"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "6811941116"))
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Kingbrukh:kingkhan@kingbruh.ra3pjgm.mongodb.net/?retryWrites=true&w=majority")
 PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001916993821"))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001983841726"))
 MCHANNEL = os.environ.get("MCHANNEL", "MuzikKayit")
+SUPPORT = os.environ.get("SUPPORT", "BotsDestek")
 CHANNELL = os.environ.get("CHANNELL", "BotsDuyuru")
 OWNERNAME = "ㅤᴀɪ‌ᴋᴏㅤ"
