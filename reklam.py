9d3e165266d80a56a11b35def6d960c4f7e8d8d0
HariboTube <57761676+HariboTube@users.noreply.github.com>
2023-11-25 07:18:50 +0300

    Update reklam.py

--- reklam.py
+++ reklam.py
@@ -8,6 +8,9 @@ from pyrogram.types.messages_and_media import Message
 from pyrogram import Client, filters
 import time
 
+import config
+from config import *
+
 import datetime
 import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
@@ -34,21 +37,21 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("APP_ID","18049084"))
-api_hash = os.environ.get("API_HASH","7e74b1e22026fcc291d32b3d431aa21e")
-bot_token = os.environ.get("TOKEN","6654452037:AAHargEKlXpvJD8tKXb5vsxCO7EnryVBWOQ")
+'''API_ID = int(os.environ.get("API_ID","18049084"))
+API_HASH = os.environ.get("API_HASH","7e74b1e22026fcc291d32b3d431aa21e")
+BOT_TOKEN = os.environ.get("TOKEN","6654452037:AAHargEKlXpvJD8tKXb5vsxCO7EnryVBWOQ")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://botrobin72:robin72@cluster0.ynhazzk.mongodb.net/?retryWrites=true&w=majority") # MongoDB veritabanınızın url'si. Nasıl alacağınızı bilmiyorsanız destek grubu @RepoHaneX'e gelin.
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "DeryaTaggerBot") # Botunuzun kullanıcı adı.
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001983841726")) # Botunuzun eylemleri kaydedeceği kayıt grubunun id'si.
 GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "BotsDestek") # Botunuzdan yasaklanan kullanıcıların itiraz işlemleri için başvuracağı grup, kanal veya kullanıcı. Boş bırakırsanız otomatik olarak OWNER_ID kimliğine yönlendirecektir.
 GONDERME_TURU = os.environ.get("GONDERME_TURU", True) #Eğer direkt iletmek isterseniz False, kopyasını göndermek isterseniz True 
 OWNER_ID = int(os.environ.get("OWNER_ID", "6811941116")) # Sahip hesabın id'si
-LANGAUGE = os.environ.get("LANGAUGE", "TR")
+LANGAUGE = os.environ.get("LANGAUGE", "TR")'''
 
 app = Client("GUNC",
-             api_id=api_id,
-             api_hash=api_hash,
-             bot_token=bot_token
+             api_id=config.API_ID,
+             api_hash=config.API_HASH,
+             bot_token=config.BOT_TOKEN
 )
 
 ################### VERİTABANI VERİ GİRİŞ ÇIKIŞI #########################
