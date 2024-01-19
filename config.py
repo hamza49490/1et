5e1a86faa2520ef93b779775cb95a883f62856c9
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2024-01-19 19:56:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,4 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5021621064"))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002124041397"))
 CHANNELL = os.environ.get("CHANNELL", "botdestekanali")
 OWNERNAME = "robin"
-PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002012164341"))
-MCHANNEL = os.environ.get("MCHANNEL", "buketplaylist")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Kingbrukh:kingkhan@kingbruh.ra3pjgm.mongodb.net/?retryWrites=true&w=majority")
-GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "SohbeTutkunu")
-GONDERME_TURU = os.environ.get("GONDERME_TURU", True) #Eğer direkt iletmek isterseniz False, kopyasını göndermek isterseniz True 
-LANGAUGE = os.environ.get("LANGAUGE", "TR")
-İTİRAF_GRUP = int(os.environ.get("İTİRAF_GRUP", "-1002020385016"))
-İTİRAF_KANAL = os.environ.get("İTİRAF_KANAL", "itirafkanali")
+SUPPORT = os.environ.get("GROUP_SUPPORT", "SohbeTutkunu")
