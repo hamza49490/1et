542ce8f10b13ba23dde687b5f5964479fdb71f42
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2024-02-15 21:08:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5021621064"))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002124041397"))
 CHANNELL = os.environ.get("CHANNELL", "botdestekanali")
 SUPPORT = os.environ.get("SUPPORT", "SohbeTutkunu")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://botrobin72:robin72@cluster0.ynhazzk.mongodb.net/?retryWrites=true&w=majority")
+GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "SohbeTutkunu")
+GONDERME_TURU = os.environ.get("GONDERME_TURU", True) #Eğer direkt iletmek isterseniz False, kopyasını göndermek isterseniz True 
+LANGAUGE = os.environ.get("LANGAUGE", "TR")
