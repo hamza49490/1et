c96cf6fb9b1629f5d2ddef4129329e0afe6c2834
Robinsayko <134005783+Robinsayko@users.noreply.github.com>
2024-04-25 14:41:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "26573250"))
-API_HASH = os.environ.get("API_HASH", "6306d2d23b1083a6f757f64f0b0c609c")
+API_ID = int(os.environ.get("API_ID", "5609667"))
+API_HASH = os.environ.get("API_HASH", "cc92701bffd8aba48ffdbc03b8ea9c1d")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "BuketTaggerBot")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6404904263:AAEHmxqBw5tZXSVkVDRFLePsoWzjSfoa77E")
 BOT_ID = int(os.environ.get("BOT_ID", "6404904263"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "5021621064"))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002124041397"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002036944773"))
 CHANNELL = os.environ.get("CHANNELL", "botdestekanali")
 SUPPORT = os.environ.get("SUPPORT", "sohbet_muhabbet_ortami")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://botrobin72:robin72@cluster0.ynhazzk.mongodb.net/?retryWrites=true&w=majority")
